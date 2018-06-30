51c9aad40cd78244e9c6fc850629730360365e06
Aardenfell <aa>
2018-06-29 22:10:57 -0500

    Bikes.cs Added + Program.cs modified

---
+++ VTBike/Bike.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VTBike
+{   /// <summary>
+    ///  Bike Object
+    /// </summary>
+    class Bike
+    {
+        /// <summary>
+        ///  Gets set number of tyres
+        /// </summary>
+        public int NumOfTyres { get; set; }
+
+        /// <summary>
+        /// Gets set number of Streamers
+        /// </summary>
+        public int NumOfStreamers { get; set; }
+
+        /// <summary>
+        /// Initialize the bike
+        /// </summary>
+        /// <param name="tyreCount">Number of tyres.</param>
+        /// <param name="streamerCount">Number of streamers.</param>
+        public Bike(int tyreCount, int streamerCount)
+        {
+            // Setup
+            this.NumOfTyres = tyreCount;
+            this.NumOfStreamers = streamerCount;
+        }
+
+
+        public string GetBikeDesc()
+        {
+            return "This bad boy has" + this.NumOfTyres + "wheels and" + this.NumOfStreamers + "streamers on it. Rad";
+        }
+        
+        public string GetTypeBike()
+        {
+            if (this.NumOfTyres < 2)
+            {
+                return "A Piece of Junk";
+            }
+            else if (this.NumOfTyres == 2)
+            {
+                return "A Normal Bike";
+            }
+            else if (this.NumOfTyres == 3)
+            {
+                return "A Tricycle";
+            }
+            else if (this.NumOfTyres > 3)
+            {
+                return "Something that isn't a bike";
+            }
+            else if (this.NumOfTyres == 2 && this.NumOfStreamers == 2)
+            {
+                return "Your mum's bike. Why are you using that thing?";
+            }
+        }
+
+        /// <summary>
+        /// Change tyres with loop
+        /// </summary>
+        public void ChangeTyresWhile()
+        {
+            if (this.NumOfTyres <= 0)
+            {
+                Console.WriteLine("No tyres need to be changed here");
+                return;
+            }
+
+            List<int> removedTyres = new List<int>();
+            int tyreNum = 0;
+
+            while (removedTyres.Count < this.NumOfTyres)
+            {
+                tyreNum++;
+                Console.WriteLine("Removing Tyre " + tyreNum);
+                removedTyres.Add(tyreNum);
+            }
+
+            do
+            {
+                Console.WriteLine("Replacing Tyre " + removedTyres[0]);
+                removedTyres.RemoveRange(0, 1);
+                tyreNum--;
+            } while (tyreNum > 0);
+        }
+    }
+}
--- VTBike/Program.cs
+++ VTBike/Program.cs
@@ -1,12 +1,326 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
 
 namespace VTBike
 {
     class Program
     {
+
+        public static class Answer
+        {
+            public static string answer { get; }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string answer;
+            int tyre; 
+            int streamers;
+
+            Console.WriteLine("Hello! Welcome to my bike shop!");
+            Console.WriteLine("My name is [Coal]");
+            Thread.Sleep(1000);
+            tryAgain:
+            Console.WriteLine("How many tyres would you like on your bike?");
+            try
+            {
+                tyre = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("That's a bit too many tyres there buddy.");
+                Console.WriteLine("Try again");
+                goto tryAgain;
+            }
+            do
+            {
+                // Junk Bike
+                if (tyre < 2)
+                {
+                    askAgain:
+                    Console.WriteLine("Would you like to add some streamers?");
+                    Console.WriteLine("[Y/N]");
+                    answer = Console.ReadLine();
+                    if (answer == "N" || answer == "n")
+                    {
+                        Console.WriteLine("Alrighty");
+                        goto junkBike;
+                    }
+                    else if (answer == "Y" || answer == "y")
+                    {
+                        tryAgainStreamers:
+                        Console.WriteLine("How many?");
+                        try
+                        {
+                            streamers = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("That's a bit too many tyres there buddy.");
+                            Console.WriteLine("Try again");
+                            goto tryAgainStreamers;
+                        }
+                        if (streamers > 0)
+                        {
+                            goto junkHasStreams; 
+                        }
+                    }
+                    else if (answer != "Y" || answer != "y" || answer != "N" || answer != "n")
+                    {
+                        Console.WriteLine("I'm sorry. I don't understand that");
+                        Thread.Sleep(1000);
+                        goto askAgain;
+                    }
+                }
+
+                // Normal Bike
+                else if (tyre == 2)
+                {
+                    askAgain:
+                    Console.WriteLine("Would you like to add some streamers?");
+                    Console.WriteLine("[Y/N]");
+                    answer = Console.ReadLine();
+                    if (answer == "N" || answer == "n")
+                    {
+                        Console.WriteLine("Alrighty");
+                        goto normalBike;
+                    }
+                    else if (answer == "Y" || answer == "y")
+                    {
+                        tryAgainStreamers:
+                        Console.WriteLine("How many?");
+                        try
+                        {
+                            streamers = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("That's a bit too many tyres there buddy.");
+                            Console.WriteLine("Try again");
+                            goto tryAgainStreamers;
+                        }
+                        if (streamers == 2)
+                        {
+                            goto normalMumsBike; 
+                        }
+                        else if (streamers >= 3)
+                        {
+                            goto normalBikeStream; 
+                        }
+                    }
+                    else if (answer != "Y" || answer != "y" || answer != "N" || answer != "n")
+                    {
+                        Console.WriteLine("I'm sorry. I don't understand that");
+                        Thread.Sleep(1000);
+                        goto askAgain;
+                    }
+                }
+
+                // Trike
+                else if (tyre == 3)
+                {
+                    askAgain:
+                    Console.WriteLine("Would you like to add some streamers?");
+                    Console.WriteLine("[Y/N]");
+                    answer = Console.ReadLine();
+                    if (answer == "N" || answer == "n")
+                    {
+                        Console.WriteLine("Alrighty");
+                        goto trikeBike;
+                    }
+                    else if (answer == "Y" || answer == "y")
+                    {
+                        tryAgainStreamers:
+                        Console.WriteLine("How many?");
+                        try
+                        {
+                            streamers = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("That's a bit too many tyres there buddy.");
+                            Console.WriteLine("Try again");
+                            goto tryAgainStreamers;
+                        }
+                        if (streamers > 0)
+                        {
+                            goto trikeHasStreams; 
+                        }
+                        
+                    }
+                    else if (answer != "Y" || answer != "y" || answer != "N" || answer != "n")
+                    {
+                        Console.WriteLine("I'm sorry. I don't understand that");
+                        Thread.Sleep(1000);
+                        goto askAgain;
+                    }
+                }
+
+                // Monster Bike
+                else if (tyre >= 4)
+                {
+                    askAgain:
+                    Console.WriteLine("Would you like to add some streamers?");
+                    Console.WriteLine("[Y/N]");
+                    answer = Console.ReadLine();
+                    if (answer == "N" || answer == "n")
+                    {
+                        Console.WriteLine("Alrighty");
+                        goto monsterBike;
+                    }
+                    else if (answer == "Y" || answer == "y")
+                    {
+                        tryAgainStreamers:
+                        Console.WriteLine("How many?");
+                        try
+                        {
+                            streamers = Convert.ToInt32(Console.ReadLine());
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("That's a bit too many tyres there buddy.");
+                            Console.WriteLine("Try again");
+                            goto tryAgainStreamers;
+                        }
+                        if (streamers > 0)
+                        {
+                            goto monsterHasStreams; 
+                        }
+                    }
+                    else if (answer != "Y" || answer != "y" || answer != "N" || answer != "n")
+                    {
+                        Console.WriteLine("I'm sorry. I don't understand that");
+                        Thread.Sleep(1000);
+                        goto askAgain;
+                    }
+                }
+            } while (answer == "Y" || answer == "y");
+
+            junkBike:
+            Console.WriteLine("Creating..");
+            Bike junker = new Bike(0, 0);
+
+            
+            junker.ChangeTyresWhile();
+            Console.WriteLine(junker.GetBikeDesc());
+            Console.WriteLine(junker.GetTypeBike());
+
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            // End
+
+            junkHasStreams:
+            Console.WriteLine("Creating..");
+            Bike junkerStreams = new Bike(0, 0);
+
+
+            junkerStreams.ChangeTyresWhile();
+            Console.WriteLine(junkerStreams.GetBikeDesc());
+            Console.WriteLine(junkerStreams.GetTypeBike());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            normalBike:
+            Console.WriteLine("Creating..");
+            Bike normal = new Bike(2, 0);
+
+
+            normal.ChangeTyresWhile();
+            Console.WriteLine(normal.GetBikeDesc());
+            Console.WriteLine(normal.GetTypeBike());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // End
+
+            normalMumsBike:
+            Console.WriteLine("Creating..");
+            Bike normalMums = new Bike(2, 2);
+
+
+            normalMums.ChangeTyresWhile();
+            Console.WriteLine(normalMums.GetBikeDesc());
+            Console.WriteLine(normalMums.GetTypeBike());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // End
+
+            normalBikeStream:
+            Console.WriteLine("Creating..");
+            Bike normalStreams = new Bike(2, 0);
+
+
+            normalStreams.ChangeTyresWhile();
+            Console.WriteLine(normalStreams.GetBikeDesc());
+            Console.WriteLine(normalStreams.GetTypeBike());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // End
+
+            trikeBike:
+            Console.WriteLine("Creating..");
+            Bike trike = new Bike(3, 0);
+
+
+            trike.ChangeTyresWhile();
+            Console.WriteLine(trike.GetBikeDesc());
+            Console.WriteLine(trike.GetTypeBike());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // End
+
+            trikeHasStreams:
+            Console.WriteLine("Creating..");
+            Bike trikeStreams = new Bike(0, 0);
+
+
+            trikeStreams.ChangeTyresWhile();
+            Console.WriteLine(trikeStreams.GetBikeDesc());
+            Console.WriteLine(trikeStreams.GetTypeBike());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // End
+
+            monsterBike:
+            Console.WriteLine("Creating..");
+            Bike monster = new Bike(0, 0);
+
+
+            monster.ChangeTyresWhile();
+            Console.WriteLine(monster.GetBikeDesc());
+            Console.WriteLine(monster.GetTypeBike());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // End
+
+            monsterHasStreams:
+            Console.WriteLine("Creating..");
+            Bike monsterStreams = new Bike(0, 0);
+
+
+            monsterStreams.ChangeTyresWhile();
+            Console.WriteLine(monsterStreams.GetBikeDesc());
+            Console.WriteLine(monsterStreams.GetTypeBike());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // End
+
         }
     }
 }
